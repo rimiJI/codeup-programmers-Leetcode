6a352518594808aaf69e06ebfa699804f8a2f651
Hyerim JI <rimirimijj@gmail.com>
2024-06-24 00:30:23 +0900

    d

--- programmers.py
+++ programmers.py
@@ -4,3 +4,14 @@ def solution(num1, num2):
     if 0 <= num1 <= 100 and 0 <= num2 <= 100:
         answer=int(num1)*int(num2)
         return answer
+
+
+def solution(n):
+    if n<=7:
+        return 1
+    else:
+        return (n//7)+1
+    
+
+n=int(input())
+print(solution(n))

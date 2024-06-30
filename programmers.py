f50e436289d0fe193d1d3ccad29b5095c3d1fce1
Hyerim JI <rimirimijj@gmail.com>
2024-06-30 16:09:18 +0900

    240630 -양꼬치

--- programmers.py
+++ programmers.py
@@ -1,17 +1,41 @@
-#day1 두수의 곱
-def solution(num1, num2):
-    answer = 0
-    if 0 <= num1 <= 100 and 0 <= num2 <= 100:
-        answer=int(num1)*int(num2)
-        return answer
+# #day1 두수의 곱
+# def solution(num1, num2):
+#     answer = 0
+#     if 0 <= num1 <= 100 and 0 <= num2 <= 100:
+#         answer=int(num1)*int(num2)
+#         return answer
 
 
-def solution(n):
-    if n<=7:
-        return 1
-    else:
-        return (n//7)+1
+# def solution(n):
+#     if n<=7:
+#         return 1
+#     else:
+#         return (n//7)+1
     
 
-n=int(input())
-print(solution(n))
+# n=int(input())
+# print(solution(n))
+
+
+
+# def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+#     if square_target < 0:
+#         raise ValueError('Square root of negative number is not defined in real numbers')
+#     if square_target == 1:
+#         root = 1
+#         print(f'The square root of {square_target} is 1')
+#     elif square_target == 0:
+#         root = 0
+#         print(f'The square root of {square_target} is 0')
+#     else:
+#         low=0
+#         high=max(1,square_target)
+
+
+#n인분 , 음료수 k개 총얼마지불?? 양꼬치 1인분 12000 , 음료수 2000
+
+def solution(n,k):
+    return n*12000+k*2000
+
+n,k=map(int,input().split())
+print(solution(n,k))

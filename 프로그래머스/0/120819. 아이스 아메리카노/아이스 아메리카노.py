29e8438d9d106645ba0beaaa772b85f697fbf682
rimiJI <94333495+rimiJI@users.noreply.github.com>
2024-07-01 12:19:09 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,6 +1,3 @@
-def solution(money): 
-    count = int(money/5500)
-    left=int(money%5500)
-    return [count,left]
-
-#아메리카노 한잔에 5500, 갖은 돈 money -> 최대로 마실 수 있는 잔의 수와 남은 돈,,,,,,,
+def solution(money):
+    answer = []
+    return answer

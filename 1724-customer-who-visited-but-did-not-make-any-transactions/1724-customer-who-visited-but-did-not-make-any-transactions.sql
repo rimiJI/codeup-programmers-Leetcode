47b4518b6c3ed64807c90da15951d23a4d3ab52c
rimiJI <94333495+rimiJI@users.noreply.github.com>
2024-08-10 17:08:36 +0900

    Time: 1345 ms (39.74%), Space: 0B (100%) - LeetHub

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,5 @@
+SELECT v.customer_id , COUNT(v.customer_id) AS count_no_trans
+FROM Visits AS v
+LEFT JOIN Transactions AS t      ON  v.visit_id= t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id;

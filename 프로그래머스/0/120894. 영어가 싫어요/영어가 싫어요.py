acc1ab96bb1105afbec2d2708c65a19e04f68289
rimiJI <94333495+rimiJI@users.noreply.github.com>
2024-08-18 15:19:47 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,5 +1,5 @@
 def solution(numbers):
-    n={ "zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+    n={ "zero":'0', "one":'1', "two":'2', "three":'3', "four":'4', "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'}
     for i in n.keys():
         numbers=numbers.replace(i,n[i])
-    return numbers
+    return int(numbers)

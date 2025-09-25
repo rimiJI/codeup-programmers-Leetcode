9c38e4bcb510e84f46e57a51f0118c687d02939e
rimiJI <94333495+rimiJI@users.noreply.github.com>
2025-09-25 15:16:59 +0900

    Time: 19 ms (55.76%), Space: 74 MB (48.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,7 +2,17 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-let containsDuplicate = function (nums) {
-  const unique = new Set(nums);
-  return unique.size !== nums.length; //오타주의 1) rength❌  2) Set에는 length 속성 없음 size
+// let containsDuplicate = function (nums) {
+//   const unique = new Set(nums);
+//   return unique.size !== nums.length; //오타주의 1) rength❌  2) Set에는 length 속성 없음 size
+// };
+
+//또 다른 방법 _ 위와 같은 원리인데 이건 중복값 발견하면 바로 true 출력
+containsDuplicate = (nums) => {
+  const unique = new Set();
+  for (v of nums) {
+    if (unique.has(v)) return true;
+    unique.add(v);
+  }
+  return false;
 };
